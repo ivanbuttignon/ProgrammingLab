5f45e391279870a1c53843a7bf08e5dffe769834
ivanbuttignon <115220120+ivanbuttignon@users.noreply.github.com>
2022-11-23 09:47:19 +0000

    aggiunto commenti

--- esercizi/es1.py
+++ esercizi/es1.py
@@ -1,3 +1,5 @@
+# LEZIONE 2
+
 # definisco una lista e sommo i suoi elementi
 
 lista = [1,3,4,7]
--- esercizi/es2.py
+++ esercizi/es2.py
@@ -1,20 +1,36 @@
+#LEZIONE 3
+
 # leggo valori file shampoo_sales, li salvo e li sommo
 
 def sum_csv(file_name):
-    values = []
-    my_file = open(file_name, 'r')
-    if len(my_file) == 0:
+# lista che contiene i valori degli shampoo venduti
+    values = [] # inizialmente vuota
+# apro il file in modalità sola lettura 'r'
+    my_file = open(file_name, 'r') 
+# se il file è vuoto ritorno None
+    if my_file == []:
         return None
+# altrimenti opero su ogni linea del file
     for line in my_file:
-        elements = line.split(',')
+# creo una lista elements facendo lo split di        ciascuna riga del file a livello della virgola 
+        elements = line.split(',') 
+# mi assicuro di non fare nulla se sono sulla          prima riga del file
         if elements[0] != 'Date':
-            date = elements[0]
+# salvo le date in una variabile che pongo quindi uguale a elements[0]
+            date = elements[0] 
+# salvo il numero degli shampoo venduti in una variabile value, convertendoli a float
             value = float(elements[1])
+# "carico" ciascun value nella lista values inizializzata in precedenza
             values.append(value)
+# calcolo la somma degli elementi di values
     sum = 0
     for item in values:
         sum = sum + item
-    return sum
+    if sum == 0:
+        return None
+    else:
+        return sum
+        
 #print('La somma dei valori di shampoo_sales è {}'.format(sum_csv('shampoo_sales.csv')))
 
     
